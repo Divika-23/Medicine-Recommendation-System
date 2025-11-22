b459d327812e252d8416e3b7e093e544b4415648
Divika-23 <divikathota@gmail.com>
2025-11-22 14:54:28 +0530

    Load datasets for symptoms, precautions, and more
    Added loading of multiple datasets for the Flask app.

--- app.py
+++ app.py
@@ -3,5 +3,15 @@ import numpy as np
 import pandas as pd
 import pickle
 
+# flask app
 app = Flask(__name__)
+
+# load databasedataset===================================
+sym_des = pd.read_csv("datasets/symtoms_df.csv")
+precautions = pd.read_csv("datasets/precautions_df.csv")
+workout = pd.read_csv("datasets/workout_df.csv")
+description = pd.read_csv("datasets/description.csv")
+medications = pd.read_csv('datasets/medications.csv')
+diets = pd.read_csv("datasets/diets.csv")
+
 
