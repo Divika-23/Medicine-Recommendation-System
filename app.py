7ce4b65c7a4b8b545211de830f4dae7300980e76
Divika-23 <divikathota@gmail.com>
2025-11-27 21:28:02 +0530

    Implement helper function for disease details
    Added a helper function to retrieve disease-related information.

--- app.py
+++ app.py
@@ -18,3 +18,25 @@ diets = pd.read_csv("datasets/diets.csv")
 # load model===========================================
 svc = pickle.load(open('models/svc.pkl','rb'))
 
+
+#============================================================
+# custome and helping functions
+#==========================helper funtions================
+def helper(dis):
+    desc = description[description['Disease'] == dis]['Description']
+    desc = " ".join([w for w in desc])
+
+    pre = precautions[precautions['Disease'] == dis][['Precaution_1', 'Precaution_2', 'Precaution_3', 'Precaution_4']]
+    pre = [col for col in pre.values]
+
+    med = medications[medications['Disease'] == dis]['Medication']
+    med = [med for med in med.values]
+
+    die = diets[diets['Disease'] == dis]['Diet']
+    die = [die for die in die.values]
+
+    wrkout = workout[workout['disease'] == dis]['workout']
+
+    return desc, pre, med, die, wrkout
+
+
