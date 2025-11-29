681fc7765dbf1b58a5d9b65f6a5faddc33ab861b
Divika-23 <divikathota@gmail.com>
2025-11-29 21:05:15 +0530

    Implement disease prediction route
    Added a new route for disease prediction based on user symptoms.

--- app.py
+++ app.py
@@ -56,3 +56,33 @@ def get_predicted_value(patient_symptoms):
 @app.route("/")
 def index():
     return render_template("index.html")
+
+# Define a route for the home page
+@app.route('/predict', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        symptoms = request.form.get('symptoms')
+        # mysysms = request.form.get('mysysms')
+        # print(mysysms)
+        print(symptoms)
+        if symptoms =="Symptoms":
+            message = "Please either write symptoms or you have written misspelled symptoms"
+            return render_template('index.html', message=message)
+        else:
+
+            # Split the user's input into a list of symptoms (assuming they are comma-separated)
+            user_symptoms = [s.strip() for s in symptoms.split(',')]
+            # Remove any extra characters, if any
+            user_symptoms = [symptom.strip("[]' ") for symptom in user_symptoms]
+            predicted_disease = get_predicted_value(user_symptoms)
+            dis_des, precautions, medications, rec_diet, workout = helper(predicted_disease)
+
+            my_precautions = []
+            for i in precautions[0]:
+                my_precautions.append(i)
+
+            return render_template('index.html', predicted_disease=predicted_disease, dis_des=dis_des,
+                                   my_precautions=my_precautions, medications=medications, my_diet=rec_diet,
+                                   workout=workout)
+
+    return render_template('index.html')
