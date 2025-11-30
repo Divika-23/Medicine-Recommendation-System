e475414778e208ce397ba321f61fbc470b9ec87e
Divika-23 <divikathota@gmail.com>
2025-11-30 18:21:56 +0530

    Add contact view function and route

--- app.py
+++ app.py
@@ -91,3 +91,9 @@ def home():
 @app.route('/about')
 def about():
     return render_template("about.html")
+
+# contact view funtion and path
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
